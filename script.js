23134e93bc47f26b82caf1c15efd724f6199e031
labankibet <laban.kibet@moringaschool.com>
2023-08-03 16:36:43 +0300

    image tags

--- script.js
+++ script.js
@@ -13,3 +13,42 @@ openShopping.addEventListener('click', ()=>{
 closeShopping.addEventListener('click', ()=>{
     body.classList.remove('active');
 })
+
+let products = [
+    {
+        id: 1,
+        name: 'PANEER BHURJI',
+        image: '1.PNG',
+        price: 1200
+    },
+    {
+        id: 2,
+        name: 'POUSINE CHICKEN',
+        image: '2.PNG',
+        price: 1000
+    },
+    {
+        id: 3,
+        name: 'KARAI VEGETABLE',
+        image: '3.PNG',
+        price: 2200
+    },
+    {
+        id: 4,
+        name: 'SPANISH OMELETTE',
+        image: '4.PNG',
+        price: 1230
+    },
+    {
+        id: 5,
+        name: 'MIXED VEGETABLE',
+        image: '5.PNG',
+        price: 700
+    },
+    {
+        id: 6,
+        name: 'ITALIAN PANEER',
+        image: '6.PNG',
+        price: 120000
+    }
+];
